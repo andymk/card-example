06615cee4c0035bb5bc4c46b4d722dd313cfebcd
Andy Knight <andym.knight@gmail.com>
2023-04-12 21:31:50 +0100

    rating display improved

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -20,12 +20,27 @@ const PropertyCard = (props: PropertyCardProps) => {
                     {property.price}
                     <span className="text-sm text-gray-600"> / night</span>
                 </div>
-                <div className="mt-4">
-                    <span className="font-semibold text-teal-500">
-                        {property.rating}/5 stars (
-                    </span>
-                    <span className="text-sm text-gray-600">
-                        based on {property.reviewCount} reviews)
+                <div className="mt-2 flex items-center">
+                    {[1, 2, 3, 4, 5].map((i) => (
+                        <svg
+                            key={i}
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            className={`${
+                                i <= property.rating
+                                    ? " text-yellow-400"
+                                    : " text-gray-400"
+                            } h-5 w-5 fill-current`}
+                        >
+                            <path
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                    ))}
+
+                    <span className="ml-2 text-sm text-gray-600">
+                        {property.reviewCount} reviews
                     </span>
                 </div>
             </div>
